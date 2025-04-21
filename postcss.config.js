93db323d8bd9610b4b9a3330b6c5b4266f162023
ookk到处流浪 <yaakua@126.com>
2025-04-21 09:00:57 +0800

    「ookk到处流浪」 at 2025-04-21 09:00:57 use fafafa.ai

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
 	plugins: {
-		'@tailwindcss/postcss': {},
+		tailwindcss: {},
 		autoprefixer: {},
-		...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-	},
-};
+	}
+}
